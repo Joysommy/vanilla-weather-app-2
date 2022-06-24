a2f681540bc6ac419b3813bff1949c77eb8cba1d
Joysommy <joyesomchi@gmail.com>
2022-06-24 10:37:09 +0100

    Showing the current temperature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data.name);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "75682493619b783a70bb68caa28efb07";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Enugu&appid=${apiKey}&units=metrics`;
+
+axios.get(apiUrl).then(displayTemperature);
